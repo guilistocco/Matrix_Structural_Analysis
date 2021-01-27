a6ed61137582738859985bf8e0e2fc54e0ce22d5
guilistocco <guilistocco@gmail.com>
2021-01-27 15:38:44 -0300

    ...

--- MATRIX STRUCTURAL ANALYSIS – THE STIFFNESS METHOD/setup.py
+++ MATRIX STRUCTURAL ANALYSIS – THE STIFFNESS METHOD/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='Matrix_Structural_Analysis',
-      version='0.1',
+      version='0.2',
       description='Matrices, attibutes and methods to Matrix Structural Analysis',
       packages=['Matrix_Structural_Analysis'],
       zip_safe=False)
